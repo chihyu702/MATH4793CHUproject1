6d98dbd30abd5554c33da7496bdebfbc332f8da8
chihyu <chih.yu.chu-1@ou.edu>
2024-04-02 12:04:00 -0500

    test theoretical univariate Normality

--- R/theoryUnivNormality.R
+++ R/theoryUnivNormality.R
@@ -1,24 +1,23 @@
 #' theoryUnivNormality
 #'
-#' @param x the dataset going to test univariate Normality through theory
+#' @param x the num vector going to test univariate Normality through theory
 #'
-#' @return
+#' @return the the list of vector proportion that within first and second interval
+#'          and if it meet the theoretical expectation
 #' @export
 #'
-#' @examples
+#' @examples theoryUnivNormality(x = rnorm(100, mean = 0, sd = 1))
 theoryUnivNormality <- function(x){
   n <- length(x)
   mean <- mean(x)
   sd <- sd(x)
 
-  # calculate the observed proportion
+  # calculate the proportion
   # within 1sd
-  p1 <- ((x >= mean - sd) + (x <= mean + sd))/n
-  cat("p1 hat = ", p1)
-  # within 2sd
-  p2 <- ((x >= mean - 2*sd) + (x <= mean+2*sd))/n
-  cat("p2 hat = ", p2)
+  p1 <-  sum(x >= mean - sd & x <= mean + sd) / n
 
+  # within 2sd
+  p2 <- sum(x >= mean - 2*sd & x <= mean + 2*sd) / n
 
   # Expected proportions under normal distribution
   expected1 <- 0.683
@@ -28,19 +27,15 @@ theoryUnivNormality <- function(x){
   test1 <- 1.396/sqrt(n)
   test2 <- 0.628/sqrt(n)
 
-  # nomality test
+  # nomality test, should show if true or false (meet the theoretical expectation or not)
   test1sd <- abs(p1-expected1) > test1
   test2sd <- abs(p2-expected2) > test2
 
-  cat("test first interval is:", test1sd)
-  cat("test second interval is:", test2sd)
-
+  # print out the result
   return(list(
-    pl_hat = p1,
+    p1_hat = p1,
     p2_hat = p2,
-    testFirstInterval = test1sd,
-    testSecondInterval = test2sd,
-  )
-
-  )
+    propWithinFirstsd= test1sd,
+    propWithinSecondsd= test1sd
+  ))
 }
