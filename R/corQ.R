991b3b7c6c6407145858279e9acb355b76458334
chihyu <chih.yu.chu-1@ou.edu>
2024-04-02 22:22:22 -0500

    QQ plot final

--- R/corQ.R
+++ R/corQ.R
@@ -1,3 +1,28 @@
-corQ <- function(x){
+#' corQ
+#'
+#' @param x the vector of the original data
+#'
+#' @return the vector of correlation coefficient of its Q-Q plot
+#' @export
+#'
+#' @examples
+corQ <- function(x) {
+  n <- length(data)
 
+  # number of distance store in d
+  # q is the vector of quantile
+  q <- numeric(n)
+  for (i in 1:n){
+    q[i] = qnorm((i-0.5)/n)
+  }
+
+  # calculate the rQ, base on 4-31
+  meanX <- mean(x)
+  meanQ <- mean(q)
+
+  up <- sum((x - meanX) * (q - meanQ))
+  denominator <- sqrt(sum((x - meanX)^2) * sum((q - meanQ)^2))
+
+  rQ <- up / denominator
+  return(rQ)
 }
